42366d4d40544f88c0c8f33ed75631f47224d641
s917421909 <917421909@qq.com>
2017-07-19 00:30:03 +0800

    Initial commit

---
+++ ttt.py
@@ -0,0 +1,28 @@
+N = int(input())# the number of couples
+# the list of couples
+C = []
+for i in range(N):
+    temp = input().split(" ")
+    C.extend(temp)
+M = int(input())# the number of guests
+L = input().split(" ")# the list of guests
+t = list(set(C) & set(L))# the same ID in guests and couples
+# the count of singles in the guests
+count = 0
+for j in range(0, 2*N, 2):
+    if C[j] in t and C[j + 1] in t:
+        count += 2
+        L.remove(C[j])
+        L.remove(C[j+1])
+
+L_new = [int(i) for i in L]
+L_new.sort()
+Number = M - count
+print(Number)
+for m in L_new:
+    if m == L_new[-1]:
+        print(m)
+    else:
+        print(m,end=' ')
+
+
