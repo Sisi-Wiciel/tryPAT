46129eb7b1d048a3ba52b2b7335148e4db0e7ca2
s917421909 <917421909@qq.com>
2017-07-19 10:31:06 +0800

    测试性能

--- ttt.py
+++ ttt.py
@@ -1,28 +1,29 @@
-N = int(input())# the number of couples
-# the list of couples
-C = []
-for i in range(N):
-    temp = input().split(" ")
-    C.extend(temp)
-M = int(input())# the number of guests
-L = input().split(" ")# the list of guests
-t = list(set(C) & set(L))# the same ID in guests and couples
-# the count of singles in the guests
-count = 0
-for j in range(0, 2*N, 2):
-    if C[j] in t and C[j + 1] in t:
-        count += 2
-        L.remove(C[j])
-        L.remove(C[j+1])
+def single():
+    N = int(input())# the number of couples
+    # the list of couples
+    C = []
+    C_extend = C.extend
+    for i in range(N):
+        temp = input().split(" ")
+        C_extend(temp)
+    M = int(input())# the number of guests
+    L = input().split(" ")# the list of guests
+    t = list(set(C) & set(L))# the same ID in guests and couples
+    # the count of singles in the guests
+    count = 0
+    for j in range(0, 2*N, 2):
+        if C[j] in t and C[j + 1] in t:
+            count += 2
+            L.remove(C[j])
+            L.remove(C[j+1])
 
-L_new = [int(i) for i in L]
-L_new.sort()
-Number = M - count
-print(Number)
-for m in L_new:
-    if m == L_new[-1]:
-        print(m)
-    else:
-        print(m,end=' ')
-
-
+    L_new = [int(i) for i in L]
+    L_new.sort()
+    Number = M - count
+    print(Number)
+    for m in L_new:
+        if m == L_new[-1]:
+            print(m)
+        else:
+            print(m,end=' ')
+single()
